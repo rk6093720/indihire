0045dcfc12efdfd386462b00546c63c0ab7c6e43
rk6093720 <97551067+rk6093720@users.noreply.github.com>
2023-06-06 20:50:09 +0530

    done
    done

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -8,14 +8,25 @@ import {  Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
-  // swiper: Swiper | undefined;
-  //    ngAfterViewInit(): void {
-  //      this.swiper = new Swiper('.swiper-container',{
-  //       navigation: {
-  //         prevEl: '.swiper-button-prev',
-  //         nextEl: '.swiper-button-next'
-  //       }
-  //      })
-  //    }
+  title = 'app'
+  image:String[]=[
+      "https://indihire.com/wp-content/uploads/2022/03/market-research.png",
+      "https://indihire.com/wp-content/uploads/2022/03/finest-talent.png",
+      "https://indihire.com/wp-content/uploads/2022/03/deep-domain.png",
+      "https://indihire.com/wp-content/uploads/2022/06/IndiHire-Icons-Copy-20.png",
+      "https://indihire.com/wp-content/uploads/2022/05/work-at-indihire.png",
+      "https://indihire.com/wp-content/uploads/2023/04/sales-hiring-01.png",
+      "https://indihire.com/wp-content/uploads/2022/05/employees.png"
+
+  ]
+  currentIndex: number = 0;
+
+  increaseIndex() {
+    this.currentIndex = (this.currentIndex + 1) % this.image.length;
+  }
+
+  decreaseIndex() {
+    this.currentIndex = (this.currentIndex - 1 + this.image.length) % this.image.length;
+  }
+
 }
