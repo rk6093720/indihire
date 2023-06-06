6fced7de1d523608e4c0668713cf225cb8cb6155
rk6093720 <97551067+rk6093720@users.noreply.github.com>
2023-06-06 18:18:43 +0530

    done
    done

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import  Swiper  from 'swiper';
+import 'swiper/swiper-bundle.css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+  swiper: Swiper | undefined;
+     ngAfterViewInit(): void {
+       this.swiper = new Swiper('.swiper-container',{
+        navigation: {
+          prevEl: '.swiper-button-prev',
+          nextEl: '.swiper-button-next'
+        }
+       })
+     }
 }
